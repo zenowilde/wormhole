036385770c5ca8dfa362bf3fd3a6c91fc3126387
wjg <jingguo.wang@bodenai.com>
2026-01-28 11:29:47 +0800

    aa

--- common/src/main/java/com/github/wormhole/common/utils/ConfigLoader.java
+++ common/src/main/java/com/github/wormhole/common/utils/ConfigLoader.java
@@ -2,6 +2,7 @@ package com.github.wormhole.common.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.ParserConfig;
 
 import org.apache.commons.io.IOUtils;
 
@@ -24,12 +25,20 @@ public class ConfigLoader {
     }
 
     public static ProxyServiceConfig parse(String data) throws IOException {
+        // 禁用 fastjson ASM，兼容 GraalVM native-image
+        ParserConfig.getGlobalInstance().setAsmEnable(false);
+
         String s = data;
         JSONObject jsonObject = JSON.parseObject(s);
         ProxyServiceConfig proxyServiceConfig = new ProxyServiceConfig();
         for (Map.Entry<String, Object> entry : jsonObject.entrySet()) {
             if (entry.getValue() instanceof  JSONObject) {
-                proxyServiceConfig.addConfig(entry.getKey(), ((JSONObject) entry.getValue()).toJavaObject(ProxyServiceConfig.ServiceConfig.class));
+                JSONObject serviceJson = (JSONObject) entry.getValue();
+                ProxyServiceConfig.ServiceConfig serviceConfig = new ProxyServiceConfig.ServiceConfig();
+                serviceConfig.setIp(serviceJson.getString("ip"));
+                serviceConfig.setPort(serviceJson.getInteger("port"));
+                serviceConfig.setMappingPort(serviceJson.getInteger("mappingPort"));
+                proxyServiceConfig.addConfig(entry.getKey(), serviceConfig);
             }
         }
         proxyServiceConfig.setServerHost(jsonObject.getString("serverHost"));
